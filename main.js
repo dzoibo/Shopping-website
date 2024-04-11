e002268ec63ea37d69fde86f81c9b245a096af57
DZOIBO IVAN <ivandztitan@gmail.com>
2024-04-11 13:11:30 +0200

    mned
    add timing function

--- main.js
+++ main.js
@@ -6,6 +6,7 @@
  */
 import './style.scss'
 import gsap from 'gsap'
+import { CustomEase } from "gsap/all";
 
 const bgs= document.querySelectorAll(".bg");
 const men = document.querySelectorAll(".men")
@@ -21,40 +22,48 @@ const bgNext= document.querySelector(".background-next");
 const bgPrev= document.querySelector(".background-prev");
 const ten = document.querySelector(".ten");
 const arrowContainer=document.querySelector(".arrow-container");
+let position =0;
 
-var position =0;
-const animationDuration=1.2;
+const animationDuration=1.5;
+const animationEase = CustomEase.create("custom", "M0,0 C0.69,0 0,1.01 1,1 ");
+
+
 
 next.addEventListener("click",()=>{
     if(position<2){
       gsap.to(bgs[position],{
             left:"-100%",
+            ease: animationEase,
             duration: animationDuration
         });
 
         gsap.to(bgs[position+1],{
             right:"0%",
-            left:"0%",
+            ease: animationEase,
             duration: animationDuration
         });
 
         gsap.to(men[position],{
             left:"-150%",
+            ease: animationEase,
             duration: animationDuration
         });
 
         gsap.to(men[position+1],{
             right:"0%",
+            ease: animationEase,
             duration: animationDuration
         });
 
         gsap.to(descs[position],{
             left:"-100%",
+            ease: animationEase,
             duration: animationDuration
         });
         gsap.to(descs[position+1],{
             right:"0%",
             left:"0px",
+            ease: animationEase,
             duration: animationDuration
         });
         position++  
@@ -65,34 +74,40 @@ prev.addEventListener("click",()=>{
     if(position!==0){
         gsap.to(bgs[position-1],{
             left:"0%",
+            ease: animationEase,
             duration: animationDuration
         });
 
         gsap.to(bgs[position],{
             right:"-150%",
             left:"initial",
+            ease: animationEase,
             duration: animationDuration
         });
 
         gsap.to(men[position-1],{
             left:"0%",
+            ease: animationEase,
             duration: animationDuration
         });
 
         gsap.to(men[position],{
             right:"-150%",
             left:"initial",
+            ease: animationEase,
             duration: animationDuration
         });
 
         gsap.to(descs[position-1],{
             left:"0%",
+            ease: animationEase,
             duration: animationDuration
 
         });
         gsap.to(descs[position],{
             right:"-150%",
             left:"initial",
+            ease: animationEase,
             duration: animationDuration
         });
 
@@ -105,16 +120,19 @@ prev.addEventListener("click",()=>{
 btQuickView.addEventListener("click",()=>{
     gsap.to(shoppingSession,{
         marginLeft:"-400px",
+        ease: animationEase,
         duration: animationDuration
     });
     gsap.to(descSession,{
         width: "50%",
+        ease: animationEase,
         duration: animationDuration
     })
    
     gsap.to(descs[3],{
         right:"0%",
         left:"0%",
+        ease: animationEase,
         duration: animationDuration
 
     });
@@ -135,6 +153,7 @@ btQuickView.addEventListener("click",()=>{
     gsap.to(descs[position],{
         left:"-150%",
         right:"initial",
+        ease: animationEase,
         duration: animationDuration
     });
 
@@ -148,6 +167,7 @@ btQuickView.addEventListener("click",()=>{
 
     gsap.to(ten,{
         scale:1.2,
+        ease: animationEase,
         duration: animationDuration
     });
 });
@@ -156,10 +176,12 @@ back.addEventListener("click",()=>{
     gsap.to(shoppingSession,{
         marginLeft:"0px",
         width: "20%",
+        ease: animationEase,
         duration: animationDuration
     });
     gsap.to(descSession,{
         width: "33.33%",
+        ease: animationEase,
         duration: animationDuration
     })
    
@@ -167,12 +189,14 @@ back.addEventListener("click",()=>{
         top:"initial",
         left:"0%",
         scale: 1,
+        ease: animationEase,
         duration: animationDuration
 
     })
     gsap.to(back,{
         opacity: 0,
-        left:"140px",
+        left:"-140px",
+        duration: 1.5
     })
     gsap.to(arrowBackground,{
         opacity:0,
@@ -181,6 +205,7 @@ back.addEventListener("click",()=>{
      gsap.to(descs[3],{
         right:"-150%",
         left: "initial",
+        ease: animationEase,
         duration: animationDuration
 
     });
@@ -188,6 +213,7 @@ back.addEventListener("click",()=>{
     gsap.to(descs[position],{
         right:"initial",
         left:"0%",
+        ease: animationEase,
         duration: animationDuration
     }); 
 
@@ -201,48 +227,60 @@ back.addEventListener("click",()=>{
 
     gsap.to(ten,{
         scale:1,
+        ease: animationEase,
         duration: animationDuration
     });
     gsap.to(men[3],{
         right:"-150%",
         left:"initial",
+        ease: animationEase,
         duration: animationDuration
     });
     gsap.to(men[4],{
         right:"-150%",
         left:"initial",
+        ease: animationEase,
         duration: animationDuration
     });
     gsap.to(men[5],{
         right:"-150%",
         left:"initial",
+        ease: animationEase,
         duration: animationDuration
     });
     
 })
 bgNext.addEventListener("click",()=>{
-    gsap.to(men[position+1],{
-        left:"0px",
+    if(position<5){
+       gsap.to(men[position+1],{
         right:"0%",
-        duration : animationDuration
-    });
+        duration : 1
+        });
 
-    gsap.to(men[position],{
-        left:"-150%",
-        duration: animationDuration
-    });
-    position++;
+        gsap.to(men[position],{
+            left:"-150%",
+            ease: animationEase,
+            duration: 1
+        });
+        position++; 
+    }
+    
 })
 bgPrev.addEventListener("click",()=>{
-    gsap.to(men[position],{
-        right:"-150%",
-        duration: animationDuration
-    });
-
-    gsap.to(men[position-1],{
-        right:"initial",
-        left:"0%",
-        duration: animationDuration
-    });
-    position--;
+    if(position>2){
+        gsap.to(men[position],{
+            right:"-150%",
+            left: "initial",
+            ease: animationEase,
+            duration: 1
+        });
+    
+        gsap.to(men[position-1],{
+            left:"0%",
+            ease: animationEase,
+            duration: 1
+        });
+        position--;
+    }
+    
 })
